10df158970230fcea38f920a68a88be6a93962d6
DESKTOP-APCS2IJ\Student <student>
2025-11-28 16:29:46 +0100

    Init

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MonsterKampfSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
